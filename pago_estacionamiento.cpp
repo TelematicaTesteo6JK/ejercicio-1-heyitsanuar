e9098f36c57d3f6bdf0151f21c541b20395cbac9
Anuar Jiménez <heyitsanuar@gmail.com>
2018-04-11 09:13:16 -0500

    Completed and tested parking test case program

--- pago_estacionamiento.cpp
+++ pago_estacionamiento.cpp
@@ -1,40 +1,91 @@
 #include <iostream>
+
+//Made, produced, and cooked by Anuar Jiménez
+
 using namespace std;
 
 /*
-Funcion que calcula la cantidad a pagar por concepto de estacionamiento
-    de 1 a 5 horas, la hora cuesta $10
-    de 6 a 10 horas, la hora cuesta $9
-    por mas de 10 horas el costo es de $7
+Function which works out the total to pay for parking
+  1 - 5 hours = $10 each
+  6 - 10 hours = $9 each
+  10 > = $7
 */
-float pago_estacionamiento(int horas)
-{
-	int cant(0);
-	if (horas>=0 && horas<=5)
-		cant = horas*10;
-	if (horas>=6 && horas<=10)
-		cant = horas*9;
-	if (horas>11)
-		cant = horas*7;
 
-	return cant;
+float parkingPayment(int hours)
+{
+  int cant(0);
+  if (hours>=0 && hours<=5)
+    cant = hours*10;
+  if (hours>=6 && hours<=10)
+    cant = hours*9;
+  if (hours>11)
+    cant = hours*7;
+
+  return cant;
 }
 
-void casos_de_prueba(){
+string hasPassed(int hour, int expected, int actual){
+    if(hour<0)
+        return "Error";
+    else
+        return (expected==actual) ? "Passed":"Failed";   
+}
 
-    cout << "TEST CASES" << endl;
-    // Agrega tus casos de prueba
+float actualPayment(int hours){
+  int cant(0);
+  
+  if (hours>0 && hours<=5)
+    cant = hours*10;
+  if (hours>=6 && hours<=10)
+    cant = hours*9;
+  if (hours>=10)
+    cant = hours*7;
 
+   return cant;
+}
 
+void workOut(){
+    int length;
+    
+    cin>>length;
+    
+    int hours[length];
+    
+    for(int i=0; i<length; i++){
+       cout<<"Enter the hours: "; 
+       cin>>hours[i];
+    }
+    
+    system("cls");
+    
+    cout<<"\n--------------------------Test cases--------------------------\n"<<endl;
+    
+    cout<<"Test data"<<"    "<<"Expected result"<<"    "<<"Actual Result"<<"    "<<"Pass or Fail\n"<<endl;
+    
+    for(int row=0; row<length; row++){
+            
+       int hour=hours[row];
+       float expected = parkingPayment(hour);
+       float actual = actualPayment(hour);
+       string result = hasPassed(hour,expected,actual); 
+       
+       cout<<"     "<<hour<<"             "<<expected<<"               "<<actual<<"             "<<result<<endl;
+    }
+    
+    cout<<endl;
+}
 
-    cout << endl;
-
+void testCases(){
+    cout<<"How many data are you gonna evaluate for Equivalence Partitioning?\n"<<endl;
+    workOut();
+    cout<<"How many data are you gonna evaluate for Boundary Value Analysis?\n"<<endl;
+    workOut();
 }
 
 int main()
 {
-
-    casos_de_prueba();
-
-	return 0;
+    testCases();
+  
+    system("pause");
+    return 0;
 }
